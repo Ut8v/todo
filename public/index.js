4302d6c7936d0ef33b160ba9e5321c5d3ca28e57
utshav khatiwada <utshavkhatiwada4@gmail.com>
2023-11-30 20:50:30 -0500

    todo list

---
+++ public/index.js
@@ -0,0 +1,81 @@
+
+
+//const deleteBtn = document.getElementById('delete-btn');
+const createBtn = document.getElementById('create-btn');
+
+
+async function createTodo(){
+try{
+    const todo = document.getElementById('createtodo').value;
+    const res =  await fetch('/create',{
+    method:'POST',
+    headers: {
+        'Content-Type': 'application/json',
+      },
+    body: JSON.stringify({
+        todo:todo
+    }) 
+})
+
+if(res.ok){
+    reload();
+}
+else{
+    console.log(res.status);
+}
+}catch(err){
+    console.log(err);
+}
+}
+createBtn.addEventListener('click', createTodo);
+
+
+
+
+
+
+
+
+
+
+
+
+const deleteBtn = document.getElementById('delete-btn');
+console.log(deleteBtn);
+async function deleteTodo() {
+    const Dtodo = document.getElementById('todo').value;
+    try{
+    const delRes =  await fetch('/delete', {
+    method: 'POST',
+    headers: {
+    'Content-Type': 'application/json'
+        },
+    body: JSON.stringify({
+    todo: Dtodo
+    })
+    });
+
+
+    if (delRes.ok) {
+         reload();
+
+    } else {
+        }
+    }catch(err){
+    }
+}
+
+deleteBtn.addEventListener('click', deleteTodo);
+
+
+
+function reload(){
+   window.location.reload();
+    console.log('reloaded');
+}
+
+
+
+
+
+
--- server.js
+++ server.js
@@ -12,9 +12,13 @@ const app= express();
 //use json
 //app.use(express.json());
 
+app.set('view engine', 'ejs');
+
 app.use(express.urlencoded({extended:true}));
 
-app.use(express.static(path.join(__dirname,'..','Frontend')));
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname,'public')));
 
 app.use(cookieParser());
 
@@ -25,12 +29,16 @@ app.use(session({
 }));
 
 app.get('/',(req,res)=>{
-    res.sendFile(path.join(__dirname, '..', 'Frontend', 'signin.html'));
+    //res.sendFile(path.join(__dirname, '..', 'Frontend', 'signin.html'));
+    res.render('signin');
 })
 
 app.get('/signup',(req,res)=>{
-    res.sendFile(path.join(__dirname, '..', 'Frontend', 'signup.html'));
+    //res.sendFile(path.join(__dirname, '..', 'Frontend', 'signup.html'));
+    res.render('signup');
 })
+
+//app.get('/')
 //sign in
 app.post('/signin',async (req,res)=>{
     
@@ -49,8 +57,11 @@ try{
 
     if(compare){
         req.session.user = {name:check.name};
-        console.log(req.session.user);
-      return res.sendFile(path.join(__dirname,'..','Frontend','home.html'));
+        req.session.userTodo = {todo:check.todo};
+        //console.log(req.session.user);
+      //return res.sendFile(path.join(__dirname,'..','Frontend','home.html'));
+      //res.render('home',{ user: req.session.user, userTodo: req.session.userTodo });
+      res.redirect('home');
     }
     else{
         return res.send('Incorrect password')
@@ -84,24 +95,25 @@ try{
         signUpData.password = hashedPassword;
         const addUser = await collection.insertMany(signUpData);
         console.log(addUser);
-       return res.send('User created successfully.');
+       return res.redirect('signin');
     }
 }catch{
   return res.send('Something went wrong')
 
 }
-    
 
 })
 
 //to create to do's
+
+
 
 app.post('/create', async (req,res)=>{
 try {
     const list = {
         // req.session.user = {name:check.name}; 
          name: req.session.user.name,
-         todo: req.body.info,
+         todo: req.body.todo,
     }
 
     console.log(list);
@@ -114,9 +126,16 @@ try {
         //console.log(createTodo);
         if (createTodo.modifiedCount > 0) {
             // If a user was found and updated
-            console.log(createTodo);
-            return res.send(`Added to the list`);
+           // console.log(createTodo);
+            //req.session.userTodo.push(list.todo);
+            const getUpdatedTodo = await collection.findOne({name: list.name});
+            req.session.userTodo = {todo:getUpdatedTodo.todo};
+            console.log(req.session.userTodo, 'Updated todo');
+            res.render('home',{ user: req.session.user, userTodo: req.session.userTodo});
         } 
+        else {
+            res.status(404);
+        }
     
     //const createTodo = await todoCollection.insertMany(list);
     //return res.send(`added to the list`);
@@ -127,6 +146,43 @@ try {
 }
 
 })
+
+//Delete todos
+
+app.post('/delete',async (req,res)=>{
+    try{
+         const userName= req.session.user.name;
+         const todo= req.body.todo;
+
+          console.log(todo);
+        
+       const removeTodo = await collection.updateOne(
+        {name:userName},
+        { $pull:{todo:todo} }
+       )
+       if (removeTodo.modifiedCount > 0) {
+
+        const getUpdatedTodo = await collection.findOne({name: userName});
+         req.session.userTodo = {todo:getUpdatedTodo.todo};
+         res.render('home',{ user: req.session.user, userTodo: req.session.userTodo});
+         console.log(req.session.userTodo, 'Updated todo');
+          console.log('deleted');
+           
+            } else {
+           //res.status(404).send("Todo not found or already deleted");
+        console.log('problem');
+    }
+
+}catch(err){
+        console.log(err);
+       res.send("Something went wrong");
+    }
+})
+
+app.get('/home', (req,res)=>{
+    res.render('home',{ user: req.session.user, userTodo: req.session.userTodo});
+})
+
 
 
 app.listen(3000, ()=>{
